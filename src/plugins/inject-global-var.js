adbc1dbb0d0a3f7fa640fe2474965f2d2a0393a9
xiaoyufei <915724526@qq.com>
2019-08-19 09:39:46 +0800

    feat : lodash 按需加载test

---
+++ src/plugins/inject-global-var.js
@@ -0,0 +1,8 @@
+// import lodash from 'lodash'
+// import {cloneDeep} from 'lodash'
+import cloneDeep from 'lodash/cloneDeep'
+
+window.glodash = {
+    // cloneDeep: lodash.cloneDeep,
+    cloneDeep,
+}
